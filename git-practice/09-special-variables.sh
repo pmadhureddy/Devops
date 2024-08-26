6373dc82ebffbc810f899ed05c856216fe3f2b3a
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-27 04:59:45 +0530

    Added all special variabled

--- git-practice/09-special-variables.sh
+++ git-practice/09-special-variables.sh
@@ -3,3 +3,18 @@
 
 
 echo "All variables passed to the script $@"
+
+echo "Number of variables passed $#"
+
+echo "Script name: $0"
+
+echo "Curent Wprking Directory $PWD"
+
+echo "Home directory of current user: $HOME"
+
+echo "Process instance id of the script is executing now : $$"
+
+sleep 100 &
+
+echo "PID of last background command: $!"
+
