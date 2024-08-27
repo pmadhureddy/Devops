6f09f95b9d28e6975905f275577baa82e364b136
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-27 05:32:52 +0530

    Modified Installation steps

--- git-practice/11-installation.sh
+++ git-practice/11-installation.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 echo "User ID is : $USERID"
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
    echo "please run this script with root privileges"
    exit 1
@@ -11,11 +11,11 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then 
     echo "mysql is not installed, going to install it"
     dnf install mysql -y
-    if [$? -ne 0 ]
+    if [ $? -ne 0 ]
     then 
        echo "My sql installation is not success. Please check it..."
     else 
