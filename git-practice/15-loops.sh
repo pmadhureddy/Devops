757ff011499c8b1b3099bd3c8361064254bb6b62
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-28 05:00:26 +0530

    Added loops to install the packages

---
+++ git-practice/15-loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+echo "User ID is : $USERID"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then echo "$2 is ..... Failed"
+         exit 1
+    else
+        echo "$2 is .... succcess"
+    fi         
+}
+
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with root privileges"
+   exit 1
+fi  
+
+#sh 15-loops.sh git mysql nginx
+for package in $@ # refers to all inputs
+do 
+   dnf list installed $package
+
+    if [ $? -ne 0 ]
+    then 
+        echo "$package is not installed, going to install it"
+        dnf install $package -y
+        VALIDATE $? "Installing $package"     
+    else 
+        echo "$package is already installed. Nothing to do ...."    
+    fi
+
+
+
+ 
+done   
