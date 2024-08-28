05d530689c4b2dacfc83885bbea69163b3a8b488
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-28 05:46:33 +0530

    Showing meesages on terminal

--- git-practice/16-redirectors.sh
+++ git-practice/16-redirectors.sh
@@ -14,21 +14,34 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e33m
+Y= "\e[33m"
+
+USAGE(){
+    echo -e  "$R USAGE: sudo sh 16-redirectots.sh package1 package2"
+    exit 1
+}
+
+if[ $# -eq 0 ]
+then 
+    USAGE
+fi
+
+echo "Script started excecuting at: $(date)"
+
 
 VALIDATE(){
 
     if [ $1 -ne 0 ]
-    then echo -e "$2 is ..... $R Failed $N" &>>$LOG_FILE
+    then echo -e "$2 is ..... $R Failed $N" | tee -a $LOG_FILE
          exit 1
     else
-        echo -e "$2 is .... $G succcess $N" &>>$LOG_FILE
+        echo -e "$2 is .... $G succcess $N"| tee -a $LOG_FILE
     fi         
 }
 
 if [ $USERID -ne 0 ]
 then
-   echo "please run this script with root privileges" &>>$LOG_FILE
+   echo "please run this script with root privileges" | tee -a $LOG_FILE
    exit 1
 fi  
 
@@ -39,11 +52,11 @@ do
 
     if [ $? -ne 0 ]
     then 
-        echo "$package is not installed, going to install it" &>>$LOG_FILE
+        echo "$package is not installed, going to install it" | tee -a $LOG_FILE
         dnf install $package -y
         VALIDATE $? "Installing $package"     
     else 
-        echo "$package is already $Y installed. Nothing to do ...."  &>>$LOG_FILE   
+        echo  -e "$package is already $Y installed. Nothing to do ...."  | tee -a $LOG_FILE   
     fi
 
 
