72fffaa0ee8dfb41328a47b0b28369d7eb758198
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 11:52:19 +0530

    Fixed the syntax error

--- git-practice/05-array.sh
+++ git-practice/05-array.sh
@@ -3,10 +3,10 @@
 
 FRUITS=('mango', 'apple', 'kiwi')
 
-echo "First fruit is: $FRUITS[1]"
-echo "Second fruit is: $FRUITS[2]"
-echo "Third fruit is: $FRUITS[3]"
+echo "First fruit is: ${FRUITS[1]}"
+echo "Second fruit is: ${FRUITS[2]}"
+echo "Third fruit is: ${FRUITS[3]}"
 
 
 
-echo "All fruit are: $FRUITS[@]"
+echo "All fruit are: ${FRUITS[@]}"
