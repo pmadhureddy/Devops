30318c70589431f18c2965f3fefe5b0f36307956
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 11:50:40 +0530

    Printing array of elements

---
+++ git-practice/05-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+FRUITS=('mango', 'apple', 'kiwi')
+
+echo "First fruit is: $FRUITS[1]"
+echo "Second fruit is: $FRUITS[2]"
+echo "Third fruit is: $FRUITS[3]"
+
+
+
+echo "All fruit are: $FRUITS[@]"
