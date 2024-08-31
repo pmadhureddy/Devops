4399d74e1304e22589e2b5687584f4150a0876c8
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 14:54:47 +0530

    Modified redirectors file

--- git-practice/16-redirectors.sh
+++ git-practice/16-redirectors.sh
@@ -3,7 +3,7 @@
 
 LOGS_FOLDER="/var/logs/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTSAMP=$(date +Y-%m-%d-%H-%M-%S)
+TIMESTSAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
 mkdir -p $LOGS_FOLDER
