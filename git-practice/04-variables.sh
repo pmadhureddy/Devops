4f3ecc09573dc7bb387585a65143326e4f885e97
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 11:43:37 +0530

    Taking username and password from the user in empty mode and reading the values and prinitng on the console

---
+++ git-practice/04-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Please enter your username::"
+
+
+read -s USERNAME
+
+echo "Username entered is: $USERNAME"
+
+
+echo  "Please enter your password::"
+
+read -s PASSWORD
+
+echo "Passwoed entered is: $PASSWORD"
