0e39b651294a7785fd766a6c9796ab970f99a958
pmadhureddy <pmadhureddy1510@gmail.com>
2024-09-01 14:03:13 +0530

    Added script to take backup by moving logs to one place to other

---
+++ git-practice/18-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+USAGE(){
+
+    echo -e "$R USAGE:: $N sh 18-bachup.sh <source> <destination> <days(optional)>"
+}
+
+# check the source  and destination are provided
+
+if [ $# =lt 2 ]
+then
+   USAGE
+fi   
